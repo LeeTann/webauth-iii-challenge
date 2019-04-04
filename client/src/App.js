408bc8b2677a633ad7d7d5f49fe9d8f77b352733
ltann <leeroyleeroy18@gmail.com>
2019-04-04 11:44:17 -0700

    MVP completed for day 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom'
 
 import './App.css';
+import Users from './users/Users'
+import Login from './login/Login'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Test</h1>
+         <nav>
+           <NavLink to="/login">Login</NavLink>
+           &nbsp;|&nbsp;
+           <NavLink to="/users">Users</NavLink>
+         </nav>
+         <header>
+           <main>
+             <Route path="/login" component={Login} />
+             <Route path="/users" component={Users} />
+           </main>
+         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
